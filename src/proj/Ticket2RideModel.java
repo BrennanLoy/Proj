f8f40e8ddd9c741bc9dbf310c29fd645c873c30e
Fronz <Fronz@10.0.1.71>
2018-10-24 23:10:02 -0700

    I messed up and wiped my work

--- src/proj/Ticket2RideModel.java
+++ src/proj/Ticket2RideModel.java
@@ -14,8 +14,17 @@ import java.util.List;
 
 public class Ticket2RideModel {
 
-    private static void ticket2RideSetup(ArrayList<Player> order, Deck d){
-        
+    private static void ticket2RideSetup(ArrayList<Player> order, Deck d, DestinationDeck dt){
+        for(int i = 0; i < 2; i++){
+            order.add(new Player("Player"+i));
+            for(int j = 0; j < 4; j++){
+                d.draw(order.get(i));
+            }
+            for(int j = 0; j < 3; j++){
+                dt.dtDraw(order.get(i));
+            }
+            
+        }
     }
     
     
@@ -34,11 +43,22 @@ public class Ticket2RideModel {
         Route r5 = new Route("Los Angeles to San Francisco", 3);
         Route r6 = new Route("Portland to San Francisco", 5);
         
-        ticket2RideSetup(turnOrder, d);
-        for(int i = 0; i < 5; i++){
-            turnOrder.get(i).pTurn(d,desDeck,r2);
+        ticket2RideSetup(turnOrder, d, desDeck);
+        for(int i = 0; i < 2; i++){
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r1));
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r2));
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r3));
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r4));
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r5));
+            System.out.println(turnOrder.get(i).validMove(d, desDeck, r6));
+
+            
             
         }
+        
+        
+        
+        
         
 
     }
